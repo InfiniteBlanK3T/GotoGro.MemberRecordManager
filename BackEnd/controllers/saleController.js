b681d1008cde3cdcc0b66128656babe916217b4f
Thomas <vo.thomas.11647@outlook.com>
2023-09-08 15:52:24 +1000

    minor updates

--- BackEnd/controllers/saleController.js
+++ BackEnd/controllers/saleController.js
@@ -1,82 +1,82 @@
 const asyncHandler = require("express-async-handler");
-const Sales = require("../models/salesModel");
+const Sales = require("../models/saleModel");
 const Sequelize = require("sequelize");
 
 //---------------API---------------
 //@access public
 //@ route GET /api/sales/
 const getAllSales = asyncHandler(async (req, res) => {
-  const sales = await Sales.findAll();
-  res.status(200).json({ sales });
+	const sales = await Sales.findAll();
+	res.status(200).json({ sales });
 });
 
 //@access public
 //@ route GET /api/sales/:id
 const getSale = asyncHandler(async (req, res) => {
-  const sale = await Sales.findByPk(req.params.id);
-  if (!sale) {
-    res.status(404);
-    throw new Error(`Sale with SaleId: \`${req.params.id}\` not found`);
-  }
+	const sale = await Sales.findByPk(req.params.id);
+	if (!sale) {
+		res.status(404);
+		throw new Error(`Sale with SaleId: \`${req.params.id}\` not found`);
+	}
 
-  res.status(200).json(sale);
+	res.status(200).json(sale);
 });
 
 //@access public
 //@ route PUT /api/sales/:id
 const updateSale = asyncHandler(async (req, res) => {
-  let sale = await Sales.findByPk(req.params.id);
-  if (!sale) {
-    res.status(404);
-    throw new Error(`Sale with SaleId: \`${req.params.id}\` not found`);
-  }
+	let sale = await Sales.findByPk(req.params.id);
+	if (!sale) {
+		res.status(404);
+		throw new Error(`Sale with SaleId: \`${req.params.id}\` not found`);
+	}
 
-  sale = await sale.update(req.body);
-  res.status(200).json(sale);
+	sale = await sale.update(req.body);
+	res.status(200).json(sale);
 });
 
 //@access public
 //@ route POST /api/sales/
 const createSale = asyncHandler(async (req, res) => {
-  const { SaleId, MemberId, ReceiptNumber, SaleDate, PaymentMethod } = req.body;
+	const { SaleId, MemberId, ReceiptNumber, SaleDate, PaymentMethod } = req.body;
 
-  try {
-    const sale = await Sales.create({
-      SaleId,
-      MemberId,
-      ReceiptNumber,
-      SaleDate,
-      PaymentMethod,
-    });
+	try {
+		const sale = await Sales.create({
+			SaleId,
+			MemberId,
+			ReceiptNumber,
+			SaleDate,
+			PaymentMethod,
+		});
 
-    res.status(201).json(sale);
-  } catch (error) {
-    if (error instanceof Sequelize.ValidationError) {
-      const messages = error.errors.map((err) => err.message);
-      return res.status(400).json({ errors: messages });
-    }
-    throw error;
-  }
+		res.status(201).json(sale);
+	} catch (error) {
+		if (error instanceof Sequelize.ValidationError) {
+			const messages = error.errors.map((err) => err.message);
+			return res.status(400).json({ errors: messages });
+		}
+		throw error;
+	}
 });
 
 //@access public
 //@ route DELETE /api/sales/:id
 const deleteSale = asyncHandler(async (req, res) => {
-  const sale = await Sales.findByPk(req.params.id);
+	const sale = await Sales.findByPk(req.params.id);
 
-  if (!sale) {
-    res.status(404);
-    throw new Error(`Sale with SaleId: \`${req.params.id}\` not found`);
-  }
+	if (!sale) {
+		res.status(404);
+		throw new Error(`Sale with SaleId: \`${req.params.id}\` not found`);
+	}
 
-  await sale.destroy();
-  res.status(200).json(sale);
+	await sale.destroy();
+	res.status(200).json(sale);
 });
 
 module.exports = {
-  getAllSales,
-  getSale,
-  updateSale,
-  createSale,
-  deleteSale,
+	getAllSales,
+	getSale,
+	updateSale,
+	createSale,
+	deleteSale,
 };
