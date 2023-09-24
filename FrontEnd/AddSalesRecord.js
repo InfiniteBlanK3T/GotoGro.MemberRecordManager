4f063921e72cc8630c91165670599b1d6e816704
Thomas <vo.thomas.11647@outlook.com>
2023-09-24 17:35:10 +1000

    FrontEnd Integration

--- FrontEnd/AddSalesRecord.js
+++ FrontEnd/AddSalesRecord.js
@@ -9,12 +9,10 @@ import { MemberIdRegEx, paymethodRegEx, ReceiptNoRegEx } from "./RegExSales.js";
 const MemberIdInput = document.getElementById("MemberId");
 const paymethodSelect = document.getElementById("paymethod");
 
-
 //spans
 
 const MemberIdSpan = document.getElementById("memberIdSpan");
 const paymethodSpan = document.getElementById("paymentMethodSpan");
-
 
 const submitButton = document.getElementById("submitButton");
 
@@ -22,8 +20,7 @@ const submitButton = document.getElementById("submitButton");
 const salesRecord = new GetSalesRecordData();
 
 const fv_MemberId = new FormValidation(MemberIdRegEx);
-const fv_paymethod = new FormValidation(paymethodRegEx); 
-
+const fv_paymethod = new FormValidation(paymethodRegEx);
 
 // declaring variables
 
@@ -32,58 +29,48 @@ let AreInputsAllValid = true;
 // function to set the user input to the GetSalesRecordData
 
 const setSalesRecord = () => {
-    salesRecord.setMemberId(MemberIdInput.value);
-    salesRecord.setpaymethod(paymethodSelect.value);
-
-}
+	salesRecord.setMemberId(MemberIdInput.value);
+	salesRecord.setpaymethod(paymethodSelect.value);
+};
 
 // function to perform form validation
 
 const formValidationFunc = () => {
-    let areInputsAllValid = true;
+	let areInputsAllValid = true;
 
-    fv_MemberId.setInput(salesRecord.getMemberId());
-    if(!fv_MemberId.isInputValid())
-    {
-        memberIdSpan.innerHTML = "Member Id must only contain numbers and letters!";
-        areInputsAllValid = false;
-    }
-    else
-    {
-        memberIdSpan.innerHTML = "";
-    }
+	fv_MemberId.setInput(salesRecord.getMemberId());
+	if (!fv_MemberId.isInputValid()) {
+		memberIdSpan.innerHTML = "Member Id must only contain numbers and letters!";
+		areInputsAllValid = false;
+	} else {
+		memberIdSpan.innerHTML = "";
+	}
 
-    fv_paymethod.setInput(salesRecord.getpaymethod());
-    if (!fv_paymethod.isInputValid()) 
-    {
-        paymentMethodSpan.innerHTML = "You must select a payment method!"; 
-        areInputsAllValid = false;
-    }
-    else
-    {
-        paymentMethodSpan.innerHTML = "";
-    }
+	fv_paymethod.setInput(salesRecord.getpaymethod());
+	if (!fv_paymethod.isInputValid()) {
+		paymentMethodSpan.innerHTML = "You must select a payment method!";
+		areInputsAllValid = false;
+	} else {
+		paymentMethodSpan.innerHTML = "";
+	}
 
+	return areInputsAllValid;
+};
 
-    return areInputsAllValid;
+const onSubmitButtonClickHandler = () => {
+	console.log("submit button clicked");
 
-}
+	setSalesRecord();
+	AreInputsAllValid = formValidationFunc();
+	if (AreInputsAllValid) {
+		//update later with form validation check
+		const salesRecordObject = {
+			MemberId: salesRecord.getMemberId(),
+			PaymentMethod: salesRecord.getpaymethod(),
+		};
+		console.log(salesRecordObject);
 
-
-const onSubmitButtonClickHandler = () => { 
-    console.log("submit button clicked");
-
-    setSalesRecord();
-    AreInputsAllValid = formValidationFunc();
-    if (AreInputsAllValid) //update later with form validation check
-    {
-        const salesRecordObject = {
-            MemberId: salesRecord.getMemberId(),
-            PaymentMethod: salesRecord.getpaymethod()
-        };
-        console.log(salesRecordObject)
-
-        fetch("http://localhost:5732/api/Sale", {
+		fetch("http://localhost:5732/api/sale", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -99,12 +86,69 @@ const onSubmitButtonClickHandler = () => {
 				alert("Error");
 				console.error("Error:", error);
 			});
-            
-        console.log(salesRecordObject);
 
-    }
-    
-    salesRecord.consoleSaleDetails();
-}
+		console.log(salesRecordObject);
+	}
+
+	salesRecord.consoleSaleDetails();
+};
 
 submitButton.onclick = onSubmitButtonClickHandler;
+
+// Debounce to reduce number of API calls
+function debounce(func, delay) {
+	let debounceTimer;
+	return function () {
+		const context = this;
+		const args = arguments;
+		clearTimeout(debounceTimer);
+		debounceTimer = setTimeout(() => func.apply(context, args), delay);
+	};
+}
+
+const debouncedSearchMembers = debounce(async function () {
+	const input = document.getElementById("searchInput");
+	const dropdown = document.getElementById("resultsDropdown");
+	const searchTerm = input.value;
+
+	if (searchTerm.length < 1) {
+		dropdown.style.display = "none";
+		return;
+	}
+
+	try {
+		const response = await fetch(
+			`http://localhost:5732/api/member/search?q=${searchTerm}`
+		);
+		const members = await response.json();
+
+		dropdown.innerHTML = "";
+
+		if (members.length === 0) {
+			dropdown.innerHTML = "<div>No results found</div>";
+		} else {
+			members.slice(0, 5).forEach((member) => {
+				const div = document.createElement("div");
+				div.innerHTML = `${member.MemberId}-${member.FirstName} ${member.LastName}`;
+				div.onclick = function () {
+					document.getElementById("MemberId").value = member.MemberId; // Fill the MemberId input
+					input.value = `${member.FirstName} ${member.LastName}`;
+					dropdown.style.display = "none"; // Hide dropdown after selection
+				};
+				dropdown.appendChild(div);
+			});
+			dropdown.style.display = "block";
+		}
+	} catch (error) {
+		dropdown.innerHTML = "<div>Error fetching results</div>";
+		console.error("Error fetching search results:", error);
+	}
+}, 300); // 300ms delay debounce
+
+//Search Member
+function searchMembers() {
+	debouncedSearchMembers();
+}
+document.getElementById("searchInput").addEventListener("keyup", searchMembers);
+
+window.searchMembers = searchMembers;
